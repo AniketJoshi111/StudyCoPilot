1988f1002fe49614988d8b9fa7b2117546770308
AniketJoshi111 <aniketjoshi2606@gmail.com>
2025-02-18 19:22:38 +0530

    first commit

---
+++ chrome-extension/background.js
@@ -0,0 +1,6 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch((error) => {
+      console.error("Failed to set panel behavior:", error);
+    });
+  });
+  
